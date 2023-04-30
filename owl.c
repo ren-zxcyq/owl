9fab078b01b0bd2dc969a8dfa3fa765500981bb1
ren-zxcyq <62415460+ren-zxcyq@users.noreply.github.com>
2023-04-30 17:13:47 +0100

    Enumerate Processes & Loaded modules using CreateToolhelp32Snapshot

--- owl.c
+++ owl.c
@@ -1,18 +1,237 @@
-#include "stdio.h"
+#include "stdio.h"	// printf() depends on this
+#include "tchar.h"	// _tprintf() depends on this (also: wide characters)
 #include "windows.h"
 #include "owl.h"
 
+#include "tlhelp32.h"
+
 void inject(int PID);
+boolean enumProcs();
+boolean listProcessModules(DWORD dwPID);
 
 
 int main(int argc, char* argv[]) {
 	// printf("Hello world %s\r\n", "!");
-	inject(7524);
+	enumProcs();
+	// inject(23816);
 	return 0;
 }
 
 
+// https://learn.microsoft.com/en-us/windows/win32/toolhelp/taking-a-snapshot-and-viewing-processes
+boolean enumProcs() {
+	// #include "tlhelp32.h"
+	/*
+		https://learn.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-createtoolhelp32snapshot
+		HANDLE CreateToolhelp32Snapshot(
+			[in] DWORD dwFlags,	// TH32CS_SNAPPROCESS -> 0x00000002 (Includes all processes in the system in the snapshot.To enumerate the processes, see Process32First.)
+			[in] DWORD th32ProcessID	// The process identifier of the process to be included in the snapshot. This parameter can be zero to indicate the current process. This parameter is used when the TH32CS_SNAPHEAPLIST, TH32CS_SNAPMODULE, TH32CS_SNAPMODULE32, or TH32CS_SNAPALL value is specified. Otherwise, it is ignored and all processes are included in the snapshot.
+		);
+		
+		Return value:
+		* If the function succeeds, it returns an open handle to the specified snapshot.
+		* If the function fails, it returns INVALID_HANDLE_VALUE. To get extended error information, call GetLastError. Possible error codes include ERROR_BAD_LENGTH.
+	*/
+	HANDLE hProcessSnap;
+	HANDLE hProcess;
 
+	hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE) {
+		printf("Failed while enumerating running processes (%d)\r\n", GetLastError());
+		return FALSE;
+	}
+
+	// Set the size of the struct before using it.
+	PROCESSENTRY32 pe32;
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	// Retrieve info about the first process
+	// exit if unsuccessful
+	/*
+		https://learn.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-process32first
+		BOOL Process32First(
+		  [in]      HANDLE           hSnapshot,	// A handle to the snapshot returned from a previous call to the CreateToolhelp32Snapshot function.
+		  [in, out] LPPROCESSENTRY32 lppe	// A pointer to a PROCESSENTRY32 structure. It contains process information such as the name of the executable file, the process identifier, and the process identifier of the parent process.
+		);
+		Return Value:
+		* Returns TRUE if the first entry of the process list has been copied to the buffer or FALSE otherwise.
+		* The ERROR_NO_MORE_FILES error value is returned by the GetLastError function if no processes exist or the snapshot does not contain process information.
+	*/
+	if (!Process32First(hProcessSnap, &pe32)) {
+		printf("First process retrieved by Process32First is invalid (%d)", GetLastError());
+		CloseHandle(hProcessSnap);
+		return FALSE;
+	}
+	
+	// Walk running processes.
+	// _tprintf("[+] Enumerating Processes:\r\n");
+	printf("[+] Enumerating Processes:\r\n");
+
+	do {
+		/*
+		typedef struct tagPROCESSENTRY32W
+		{
+			DWORD   dwSize;
+			DWORD   cntUsage;
+			DWORD   th32ProcessID;          // this process
+			ULONG_PTR th32DefaultHeapID;
+			DWORD   th32ModuleID;           // associated exe
+			DWORD   cntThreads;
+			DWORD   th32ParentProcessID;    // this process's parent process
+			LONG    pcPriClassBase;         // Base priority of process's threads
+			DWORD   dwFlags;
+			WCHAR   szExeFile[MAX_PATH];    // Path
+		} PROCESSENTRY32W;
+		*/
+		//_tprintf("[*] Process PID: %d\r\n", pe32.th32ModuleID);
+		//_tprintf("\tProcess ParentPID: %d\r\n", pe32.th32ParentProcessID);
+		//_tprintf("\tProcess' associated exe: %s\r\n", pe32.szExeFile);
+		//_tprintf("\tProcess' thread count: %d\r\n", pe32.cntThreads);
+		//_tprintf("\tProcess' dwFlags: %d\r\n", pe32.dwFlags);
+
+
+
+
+		printf("[+] ======================\r\n");
+		printf("[*] Process PID: %d\r\n", pe32.th32ProcessID);
+		printf("\tProcess' associated exe: %ws\r\n", pe32.szExeFile);
+		DWORD dwPriorityClass = 0;
+
+		/*
+			// https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-openprocess
+			// * Process Access Rights: https://learn.microsoft.com/en-us/windows/desktop/ProcThread/process-security-and-access-rights
+			// * SeDebugPrivilege Privilege: https://learn.microsoft.com/en-us/windows/win32/secauthz/privilege-constants#SE_DEBUG_NAME
+
+			HANDLE OpenProcess(
+			  [in] DWORD dwDesiredAccess,
+			  [in] BOOL  bInheritHandle,
+			  [in] DWORD dwProcessId
+			);	
+
+			Return value:
+			* If the function succeeds, the return value is an open handle to the specified process.
+			* If the function fails, the return value is NULL. To get extended error information, call GetLastError.
+		*/
+		hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, pe32.th32ProcessID);
+		// hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+
+		if (hProcess == NULL) {
+			printf("\tError while attempting to OpenProcess -> %d", pe32.th32ProcessID);
+		} else {
+			dwPriorityClass = GetPriorityClass(hProcess);
+			if (!dwPriorityClass) {
+				printf("\tError in GetPriorityClass()");
+				// hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, pe32.th32ProcessID);
+				CloseHandle(hProcess);
+			}
+		}
+
+
+		printf("\tProcess ParentPID: %d\r\n", pe32.th32ParentProcessID);
+		printf("\tProcess' thread count: %d\r\n", pe32.cntThreads);
+		// printf("\tProcesses' base thread prio: %d\r\n", pe32.pcPriClassBase);
+		printf("\tProcess' dwFlags: %d\r\n", pe32.dwFlags);
+
+
+		// To read module info we need to OpenProcess
+
+		listProcessModules(pe32.th32ProcessID);
+
+		printf("[+] -----------------------\r\n");
+
+
+
+	} while(Process32Next(hProcessSnap, &pe32));
+
+	CloseHandle(hProcessSnap);
+
+
+	gets();
+	return TRUE;
+}
+
+boolean listProcessModules(DWORD dwPID) {
+	HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
+	/*
+		// https://learn.microsoft.com/en-us/windows/win32/api/tlhelp32/ns-tlhelp32-moduleentry32
+		typedef struct tagMODULEENTRY32 {
+		  DWORD   dwSize;	// The size of the structure, in bytes. Before calling the Module32First function, set this member to sizeof(MODULEENTRY32). If you do not initialize dwSize, Module32First fails.
+		  DWORD   th32ModuleID;	// This member is no longer used, and is always set to one.
+		  DWORD   th32ProcessID;	// The identifier of the process whose modules are to be examined.
+		  DWORD   GlblcntUsage;	// The load count of the module, which is not generally meaningful, and usually equal to 0xFFFF.
+		  DWORD   ProccntUsage;	// The load count of the module (same as GlblcntUsage), which is not generally meaningful, and usually equal to 0xFFFF.
+		  BYTE    *modBaseAddr;	// The base address of the module in the context of the owning process.
+		  DWORD   modBaseSize;	// The size of the module, in bytes.
+		  HMODULE hModule;	// A handle to the module in the context of the owning process.
+		  char    szModule[MAX_MODULE_NAME32 + 1];	// The module name.
+		  char    szExePath[MAX_PATH];	// The module path.
+		} MODULEENTRY32;
+	*/
+	MODULEENTRY32 me32;
+
+	// Take a snapshot of all modules in the specified process.
+	hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
+	if (hModuleSnap == INVALID_HANDLE_VALUE) {
+		printf("\tAn error occurred while enumerating modules of Process w PID: (%d)\r\n", dwPID);
+		printf("\tError: (%d)\r\n", GetLastError());
+		// CloseHandle(hModuleSnap);
+		return FALSE;
+	}
+
+	// Set the size of the structure before using it.
+	me32.dwSize = sizeof(MODULEENTRY32);
+
+	// Retrieve info about the first module and exit if unsuccessful.
+	if (!Module32First(hModuleSnap, &me32)) {
+		printf("\tError Occurred while trying to read modules of PID: (%d)\r\n", dwPID);
+		CloseHandle(hModuleSnap);
+		return FALSE;
+	}
+
+
+	// Walk the module list
+	do {
+
+		/*
+			typedef struct tagMODULEENTRY32W
+			{
+				DWORD   dwSize;
+				DWORD   th32ModuleID;       // This module
+				DWORD   th32ProcessID;      // owning process
+				DWORD   GlblcntUsage;       // Global usage count on the module
+				DWORD   ProccntUsage;       // Module usage count in th32ProcessID's context
+				BYTE* modBaseAddr;        // Base address of module in th32ProcessID's context
+				DWORD   modBaseSize;        // Size in bytes of module starting at modBaseAddr
+				HMODULE hModule;            // The hModule of this module in th32ProcessID's context
+				WCHAR   szModule[MAX_MODULE_NAME32 + 1];
+				WCHAR   szExePath[MAX_PATH];
+			} MODULEENTRY32W;
+			typedef MODULEENTRY32W* PMODULEENTRY32W;
+			typedef MODULEENTRY32W* LPMODULEENTRY32W;
+		*/
+
+		printf("\t----------\r\n");
+		printf("\tModule Name: %ws\r\n", me32.szModule);
+		printf("\tModule Exe/PE Path: %ws\r\n", me32.szExePath);
+		// printf("\tModule module ID: %d\r\n", me32.th32ModuleID);	// No longer used, and is always set to 1
+		printf("\tModule base addr: 0x%08x\r\n", me32.modBaseAddr);	// The base address of the module in the context of the owning process.
+		printf("\tModule base size (hex): 0x%x\r\n", me32.modBaseSize);	// The size of the module, in bytes.
+		printf("\tModule base size (dec): %d\r\n", me32.modBaseSize);
+		// printf("\tModule being used (0x%04x) times by Process\r\n", me32.ProccntUsage);	// usually 0xFFFF
+		// printf("\tModule being used (0x%04x) times system wide\r\n", me32.GlblcntUsage);	// usually 0xFFFF
+		printf("\t----------\r\n");
+
+	} while(Module32Next(hModuleSnap, &me32));
+
+	CloseHandle(hModuleSnap);
+	return TRUE;
+}
+
+/*
+	Remote Process injection
+	PID -> inject shellcode into process with PID & run CreateRemoteThread
+	@TODO: Add checks to make requesting memory page perms more granular. -> W instead of RWX & then X
+*/
 void inject(int PID) {
 	printf("[+] Inj to PID: %d\r\n", PID);
 
@@ -40,6 +259,7 @@ void inject(int PID) {
 		[in]           DWORD  flProtect	// PAGE_EXECUTE_READWRITE OR 0x40
 	);
 	*/
+	// Run notepad.exe -> encoded with x64 msfvenom encoder; --platform windows --arch x64
 	unsigned char shellcode[] = "\x54\x41\x5e\x48\x31\xd2\x49\xbd\x91\x5f\xe2\x61\xe6\x27"
 								"\x6d\xf1\xd9\xcc\xb2\x23\x66\x41\x81\xe6\xf0\xf7\x49\x0f"
 								"\xae\x06\x49\x83\xc6\x08\x4d\x8b\x26\x48\xff\xca\x4d\x31"
